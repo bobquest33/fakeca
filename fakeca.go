47a5d1a4332048a816387c7a790d749d19b11423
Naitik Shah <n@daaku.org>
2014-08-14 16:27:13 -0700

    initial logic to support generating certs using the root

--- fakeca.go
+++ fakeca.go
@@ -19,7 +19,6 @@ import (
 	"time"
 
 	"github.com/facebookgo/stackerr"
-	"github.com/kr/pretty"
 )
 
 type Generator struct {
@@ -36,9 +35,16 @@ func (g *Generator) keyFileName() string {
 	return filepath.Join(g.RootLocation, g.RootName+" Key.pem")
 }
 
-func (g *Generator) genCACert() error {
+func (g *Generator) genRootCA() error {
 	certFileName := g.certFileName()
 	keyFileName := g.keyFileName()
+
+	if _, err := os.Stat(certFileName); !os.IsNotExist(err) {
+		return stackerr.Newf("%s must not exist", certFileName)
+	}
+	if _, err := os.Stat(keyFileName); !os.IsNotExist(err) {
+		return stackerr.Newf("%s must not exist", keyFileName)
+	}
 
 	priv, err := rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
@@ -87,22 +93,103 @@ func (g *Generator) genCACert() error {
 	return nil
 }
 
-func (g *Generator) showCert() {
-	certFileName := g.certFileName()
-
-	certIn, err := ioutil.ReadFile(certFileName)
+func (g *Generator) loadPem(filename string) (*pem.Block, error) {
+	in, err := ioutil.ReadFile(filename)
 	if err != nil {
-		log.Fatalf("Failed to open "+certFileName+" for reading: %s", err)
+		return nil, stackerr.Wrap(err)
 	}
-	b, _ := pem.Decode(certIn)
+	b, _ := pem.Decode(in)
 	if b == nil {
-		log.Fatalf("Failed to find a certificate in " + certFileName)
+		return nil, stackerr.Newf("failed to pem decode %s", filename)
 	}
-	caCert, err := x509.ParseCertificate(b.Bytes)
+	return b, nil
+}
+
+func (g *Generator) loadRootCA() (*x509.Certificate, error) {
+	p, err := g.loadPem(g.certFileName())
 	if err != nil {
-		log.Fatalf("Failed to parse certificate in " + certFileName)
+		return nil, err
 	}
-	pretty.Printf("%# v\n", caCert)
+
+	cert, err := x509.ParseCertificate(p.Bytes)
+	if err != nil {
+		return nil, stackerr.Wrap(err)
+	}
+	return cert, nil
+}
+
+func (g *Generator) loadRootCAKey() (*rsa.PrivateKey, error) {
+	p, err := g.loadPem(g.keyFileName())
+	if err != nil {
+		return nil, err
+	}
+
+	key, err := x509.ParsePKCS1PrivateKey(p.Bytes)
+	if err != nil {
+		return nil, stackerr.Wrap(err)
+	}
+	return key, nil
+}
+
+func (g *Generator) genForNames(names string) error {
+	certFileName := "cert.pem"
+	keyFileName := "key.pem"
+
+	rootCert, err := g.loadRootCA()
+	if err != nil {
+		return err
+	}
+
+	rootKey, err := g.loadRootCAKey()
+	if err != nil {
+		return err
+	}
+
+	priv, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return stackerr.Wrap(err)
+	}
+
+	now := time.Now()
+
+	template := x509.Certificate{
+		SerialNumber: new(big.Int).SetInt64(0),
+		Subject: pkix.Name{
+			CommonName: names,
+		},
+		NotBefore:             now.Add(-5 * time.Minute).UTC(),
+		NotAfter:              now.Add(g.MaxAge),
+		SubjectKeyId:          []byte{1, 2, 3, 4},
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		BasicConstraintsValid: true,
+	}
+
+	derBytes, err := x509.CreateCertificate(
+		rand.Reader, &template, rootCert, &priv.PublicKey, rootKey)
+	if err != nil {
+		return stackerr.Wrap(err)
+	}
+
+	certOut, err := os.Create(certFileName)
+	if err != nil {
+		return stackerr.Wrap(err)
+	}
+	pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
+	certOut.Close()
+	log.Print("Written " + certFileName + "\n")
+
+	keyOut, err := os.OpenFile(keyFileName, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return stackerr.Wrap(err)
+	}
+	pem.Encode(keyOut, &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(priv),
+	})
+	keyOut.Close()
+	log.Print("Written " + keyFileName + "\n")
+	return nil
+	return nil
 }
 
 func defaultLocation() string {
@@ -114,9 +201,7 @@ func defaultLocation() string {
 }
 
 func main() {
-	g := Generator{
-		MaxAge: time.Hour * 24 * 365 * 10,
-	}
+	g := Generator{MaxAge: time.Hour * 24 * 365 * 10}
 
 	flag.StringVar(
 		&g.RootLocation,
@@ -129,10 +214,25 @@ func main() {
 		strings.Title(os.Getenv("USER")+" CA"),
 		"The name used for various purposes.")
 
+	genRoot := flag.Bool("root", false, "generate root certificate and key")
+	genNames := flag.String("names", "", "generate certificate with given names")
+
 	flag.Parse()
 
-	if err := g.genCACert(); err != nil {
-		log.Fatal(err)
+	if !*genRoot && *genNames == "" {
+		flag.Usage()
+		os.Exit(1)
 	}
-	g.showCert()
+
+	if *genRoot {
+		if err := g.genRootCA(); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	if *genNames != "" {
+		if err := g.genForNames(*genNames); err != nil {
+			log.Fatal(err)
+		}
+	}
 }
